ac99a35c9a14b2ab33550a027acb36272ae72303
shitohana <kyudytskiy@gmail.com>
2023-12-09 13:23:55 +0300

    dumpv2

--- Dump.py
+++ Dump.py
@@ -1,105 +1,62 @@
 import asyncio
-import aiohttp
+import itertools
+import time
 from xml.etree import ElementTree as ET
-from itertools import chain
-import xmltodict
 import pandas as pd
+import aiohttp
+from collections import namedtuple
+
+import xmltodict
+
+API_LIMIT = 7
+NO_API_LIMIT = 2
+
+RequestItem = namedtuple("RequestItem", {"url", "params"}, defaults=(None,) * 2)
 
 
 class Dump:
     def __init__(self, api_key: str = None):
-        """
-        :param api_key: entrez api key -> allows maximum of 10 simultaneous requests per second
-        """
-        self.api_key = api_key
-        self.session = aiohttp.ClientSession()
+        self.__session = aiohttp.ClientSession()
+        self.__semaphore = asyncio.BoundedSemaphore(API_LIMIT if api_key is not None else NO_API_LIMIT)
+        self.__api_key = api_key
 
-        self.__rate_limit = 3 if self.api_key is None else 10
-        self.__sem = asyncio.Semaphore(self.__rate_limit)
+    async def __get(self, item: RequestItem, limit=0.0):
+        async with self.__semaphore:
+            start = time.time()
+            response = await self.__session.get(item.url, params=item.params)
+            end = time.time()
 
-    async def search_sra(self, term):
-        """
-        Search for study ids
-        :param term: search string in entrez format: e.g. "SRR123+SRR128"
-        :return: list of ids
-        """
-        url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-        params = {
-            "db": "sra",
-            "term": term,
-        }
-        if self.api_key is not None:
-            params["api_key"] = self.api_key
+            if end - start < limit:
+                await asyncio.sleep(end - start)
+            return response
 
-        async with self.__sem:
-            async with self.session.get(url, params=params) as resp:
-                if resp.status == 200:
-                    print("Search  ", term)
-                    xml = ET.fromstring(await resp.text())  # get xml response
-                    ids = [int(element.text) for element in xml.findall(".//IdList/Id")]  # get ids from xml
-                    if ids:
-                        await asyncio.sleep(1)  # for rate limit per second
-                        return ids
-                    else:
-                        raise Exception("Empty response")
-                raise Exception(f"Status is {resp.status}")
+    async def __run(self, items: list[RequestItem]):
+        test = await self.__get(items[0])
 
-    async def fetch_sra(self, term: str):
-        """
-        Search for study ids
-        :param term: comma separated ids
-        :return: list of dict
-        """
-        url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
-        params = {
-            "db": "sra",
-            "id": term
-        }
-        if self.api_key is not None:
-            params["api_key"] = self.api_key
+        tasks = await self.__retry_till_success(self.__get, [(item, 1.0) for item in items])
 
-        async with self.__sem:
-            async with self.session.get(url, params=params) as resp:
-                if resp.status == 200:
-                    print("Fetching ", term)
-                    data = xmltodict.parse(await resp.text())  # parse xml, turn to dict
-                    try:
-                        await asyncio.sleep(1)  # for rate limit per second
-                        return data["EXPERIMENT_PACKAGE_SET"]["EXPERIMENT_PACKAGE"]
-                    except KeyError:
-                        raise Exception(f"Bad response for {resp.raw_url}")
-                raise Exception(f"Status is {resp.status}")
+        return tasks
 
-    async def fetch_list(self, ids, batch_size=10):
-        """
-        :param ids: entrez ids
-        :param batch_size: ids in one request (200 is max)
-        :return DataFrame with merged responses
-        """
-        # split ids to batches for less requests
-        batches = [ids[i:i + batch_size] for i in range(0, len(ids), batch_size)]
-        # prepare search strings from batches
-        terms = [",".join([str(value) for value in batch]) for batch in batches]
-        # try untill all success
-        result = await self.__retry_till_success(self.fetch_sra, [[term] for term in terms])
-        # normalize and concat into df
-        return pd.concat([pd.json_normalize(res) for res in list(chain(*result))])
+    def __search_wrapper(self, db, term):
+        item = RequestItem(
+            url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi",
+            params=self.__add_api_key({"db": db, "term": term})
+        )
+        return item
 
-    async def search_list(self, ids, batch_size=10):
-        """
-        :param ids: study ids e.g. SRA123456
-        :param batch_size: ids in one request limit
-        :return list with ids
-        """
-        # filter empty string
-        ids = list(filter(lambda value: value, ids))
-        # split ids to batches for fewer requests
-        batches = [ids[i:i + batch_size] for i in range(0, len(ids), batch_size)]
-        # prepare search strings from batches
-        terms = [" OR ".join([str(value) for value in batch]) for batch in batches]
-        # try until all success
-        result = await self.__retry_till_success(self.search_sra, [[term] for term in terms])
-        return list(chain(*result))
+    def __fetch_wrapper(self, db, id, rettype="xml"):
+        item = RequestItem(
+            url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi",
+            params=self.__add_api_key({"db": db, "id": id, "rettype": rettype})
+        )
+        return item
+
+    def __add_api_key(self, params: dict):
+        return params | {"api_key": self.__api_key}
+
+    @staticmethod
+    def __batch_ids(ids: list, batch_size: int, sep=","):
+        return [sep.join(map(str, ids[i:i+batch_size])) for i in range(0, len(ids), batch_size)]
 
     @staticmethod
     async def __retry_till_success(coro, args_list):
@@ -123,36 +80,77 @@ class Dump:
 
         return result
 
+    async def search_query(self, db, term: str | list, batch_size=1, sep = "+"):
+        queries = self.__batch_ids(
+            [term] if type(term) is not list else term,
+            batch_size, sep
+        )
+        items = [self.__search_wrapper(db, query) for query in queries]
+
+        return await self.__run(items)
+
+    async def fetch_query(self, db, id, rettype="xml", batch_size=10):
+        queries = self.__batch_ids(
+            [id] if type(id) is not list else id,
+            batch_size, ","
+        )
+        items = [self.__fetch_wrapper(db, query, rettype) for query in queries]
+
+        return await self.__run(items)
+
+    async def close(self):
+        await self.__session.close()
+
+
+def parse_sra_ids(text: str):
+    xml = ET.fromstring(text)
+    return [element.text for element in xml.findall(".//IdList/Id")]
+
+
+def parse_fetch_result(text: str):
+    converted = xmltodict.parse(text)
+    experiments = converted["EXPERIMENT_PACKAGE_SET"]["EXPERIMENT_PACKAGE"]
+    if experiments is not None:
+        return [pd.json_normalize(experiment) for experiment in experiments]
+    else:
+        return None
+
+
+
+########################################################################################################################
+
 
 def parse_attributes(df: pd.DataFrame):
-    """
-    unnest tag column
-    """
     tag_col = 'SAMPLE.SAMPLE_ATTRIBUTES.SAMPLE_ATTRIBUTE'
-    # tag columns is a list of dictionaries, so we firstly
     # separate list into rows
     exploded = df.explode(tag_col)
     # separate dict values to columns
     exploded = pd.concat([exploded, exploded[tag_col].apply(pd.Series)])
     # delete unparsed tag column
-    return exploded.drop(columns = tag_col)
+    return exploded.drop(columns=tag_col)
 
 
-# USAGE
+async def dump_sra(filepath: str):
+    with open(filepath, "r") as file:
+        srp_list = file.read().split("\n")
+    api = Dump(api_key="3358b18abd611927981172611e5ce915bc08")
+
+    search_results = await api.search_query("sra", term=srp_list, batch_size=10, sep=" OR ")
+
+    ncbi_ids = list(itertools.chain(*[parse_sra_ids(await resp.text()) for resp in search_results]))
+
+    fetch_results = await api.fetch_query("sra", id=ncbi_ids, batch_size=50)
+
+    experiments = list(itertools.chain(*[parse_fetch_result(await resp.text()) for resp in fetch_results]))
+    df = pd.concat(experiments)
+
+    await api.close()
+    return df
+
+
 if __name__ == '__main__':
-    async def main():
-        # create class
-        parser = Dump()
-        with open("TE_SRP_list.txt", "r") as file:
-            srp_list = file.read().split("\n")
-        # get ids of experiments from selected studies
-        ids = await parser.search_list(srp_list)
-        # gather metadata of experiments into dataframe
-        experiments = await parser.fetch_list(ids)
-        # parse tag column into separate columns (if needed)
-        parsed = parse_attributes(experiments)
-        # close aiohttp session
-        await parser.session.close()
+    df = asyncio.run(dump_sra("TE_SRP_list.txt"))
 
+    df_expanded = parse_attributes(df)
 
-    asyncio.run(main())
+    pass
